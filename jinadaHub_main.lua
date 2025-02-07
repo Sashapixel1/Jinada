18391d97ad7d688284d81b57f36e69d99823c0dd
Jinada <141932118+Sashapixel1@users.noreply.github.com>
2025-02-08 00:18:55 +0800

    Update jinadaHub_main.lua

--- jinadaHub_main.lua
+++ jinadaHub_main.lua
@@ -436,3 +436,11 @@ function CheckQ()
 		end
 	end
 end;
+L_35_.Setting:Toggle("White Screen", true, function(L_244_arg0)
+	_25msShared.WhiteScreen = L_244_arg0;
+	if _25msShared.WhiteScreen then
+		L_8_:Set3dRenderingEnabled(true)
+	else
+		L_8_:Set3dRenderingEnabled(true)
+	end
+end);
