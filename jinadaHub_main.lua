fd21ef0ddc73e63dd385210b891a5c0a85fe2fae
Jinada <141932118+Sashapixel1@users.noreply.github.com>
2025-02-08 00:38:25 +0800

    Update jinadaHub_main.lua

--- jinadaHub_main.lua
+++ jinadaHub_main.lua
@@ -1,1 +1,14 @@
-game:GetService("Lighting").ClockTime = 1
+getgenv().Jinada=true
+
+
+local L_8_ = game:GetService("RunService")
+spawn(function()
+	while task.wait() do
+		if _25msShared.AlwaysDay then
+			L_8_.Heartbeat:wait()
+			do
+				game:GetService("Lighting").ClockTime = 12
+			end
+		end
+	end
+end)
